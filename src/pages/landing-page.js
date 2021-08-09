ce158732e74efca6463b4997de2457d9abc1bd38
Ditebogo Mothata <ditebogo@digitalpath.co.za>
2021-08-09 13:02:04 +0200

    Initial Commit with all files

---
+++ src/pages/landing-page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './landing-page.scss';
+
+
+const LandingPage = () => {
+
+  return (
+    <div className='landing-page-wrapper'>
+      <div className='header-bar-container'>
+        <img className='profile-icon' src='https://e-cdns-images.dzcdn.net/images/user//32x32-000000-80-0-0.jpg'/>
+        <input type='text' />
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
